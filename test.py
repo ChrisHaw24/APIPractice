86c5c945bd5e03c22d9476c6555b0eb14505c56e
chrishaw24 <chrishawthorne14@gmail.com>
2025-09-04 18:35:54 -0500

    fix csv file printing

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
 import os
 from dotenv import load_dotenv
 
+
+import csv
+
+
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
-print(API_KEY)
+with open('personal_favorites.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    header = next(reader)
+    for row in reader:
+        title = row[1]
+        mov_id = row[0]
+
+
+
+#choice = input("Pick a movie from above: ")
+#if choice == row[1]:
+ #   movie = row[1]
+  #  print(f"your choice is {movie}")
+#else:
+#    print("invalid choice")
+
+
+
+
+print(title)
+print(mov_id)
